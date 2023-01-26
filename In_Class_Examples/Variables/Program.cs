6d1b7d7d88e36593621dec36773d464b20ea20fe
Ackerman, Adam P <adamackrmn@gmail.com>
2023-01-26 17:03:59 -0600

    Completed an example over variables

--- In_Class_Examples/Variables/Program.cs
+++ In_Class_Examples/Variables/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string message = "Welcome, to my application";
+Console.WriteLine(message);
+Console.WriteLine("What is your name? >>\n");
+
+string name = Console.ReadLine();
+
+Console.WriteLine(name + ", what is your monthly income? >>");
+string answer = Console.ReadLine();
+double income = Convert.ToDouble(answer);
+
+Console.WriteLine($"{name}, what is your monthly expenses? >>");
+answer = Console.ReadLine();
+double expenses = Convert.ToDouble(answer);
+
+double total = income - expenses;
+
+Console.WriteLine($"Your total is {total.ToString("C3")}");
