6af8c4a7a9a756aa92c15d998e165d95873584a6
Ackerman, Adam P <adamackrmn@gmail.com>
2023-02-07 17:33:53 -0600

    Did some in class stuff around Loops

---
+++ In_Class_Examples/Loops_Example1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+int countBy;
+Console.WriteLine("What # do you want to count by? >>");
+string answer = Console.ReadLine();
+
+bool isUserInputReallyANumber;
+
+isUserInputReallyANumber = int.TryParse(answer, out  countBy);
+
+if (isUserInputReallyANumber == false)
+{
+    Console.WriteLine("Sorry, you failed to input a number.  Goodbye");
+    Environment.Exit(1);
+    return;
+}
+
+Console.WriteLine("What # do you want to count to? >>");
+int countTo = Convert.ToInt32(Console.ReadLine());
+
+int total = countBy;
+Console.WriteLine("While Loop");
+while (total <= countTo)
+{
+    Console.WriteLine(total.ToString("N"));
+    total = total + countBy;
+}
+
+Console.WriteLine("\nFor Loop");
+for (int i = countBy; i <= countTo; i += countBy)
+{
+    Console.WriteLine(i.ToString("N0"));
+}
---
+++ In_Class_Examples/Loops_Example2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("What # do you want to count down from? >>");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = number; i > -1; i = i - 1)
+{
+    Console.WriteLine(i.ToString("N0"));
+}
