ffe7c9890e727fc53089f91fd367d7b4de78a791
Ackerman, Adam P <adamackrmn@gmail.com>
2023-02-07 16:50:08 -0600

    Completed coin toss participation

---
+++ Participations/Conditional_CoinToss/Program.cs
@@ -0,0 +1,32 @@
+﻿Random random = new Random();
+int randomNumber = random.Next(1, 3);
+
+//Console.WriteLine(randomNumber.ToString("N0"));
+
+Console.WriteLine("Guess, Heads or Tails? >>");
+string answer = Console.ReadLine();
+
+
+string randomNumberConvertToWord;
+
+if (randomNumber == 1)
+{
+    //Console.WriteLine("It was Heads.");
+    randomNumberConvertToWord = "Heads";
+}
+else// if (randomNumber == 2)
+{
+    //Console.WriteLine("It was Tails.");
+    randomNumberConvertToWord = "Tails";
+}
+
+Console.WriteLine($"It was {randomNumberConvertToWord}.");
+
+if (randomNumberConvertToWord == answer)
+{
+    Console.WriteLine("Congratulations, you guessed correctly!");
+}
+else
+{
+    Console.WriteLine("Sorry, better luck next time.");
+}
