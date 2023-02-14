08118796fbed020ce013e79b05cc5a37726c8963
Ackerman, Adam P <adamackrmn@gmail.com>
2023-02-14 16:59:07 -0600

    DId an example over reading in a file

---
+++ In_Class_Examples/Loops_ReadInAFile/Program.cs
@@ -0,0 +1,20 @@
+﻿string entireBook = File.ReadAllText("The stainless steel rat by Harry Harrison.txt");
+
+//Console.WriteLine(entireBook);
+
+string[] linesOfBook = File.ReadAllLines("The stainless steel rat by Harry Harrison.txt");
+
+Console.WriteLine(linesOfBook[0]);
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+Console.WriteLine(linesOfBook[1]);
+
+for (int i = 0; i < linesOfBook.Length; i++)
+{
+    Console.WriteLine(linesOfBook[i]);
+	if (i % 15 == 0)
+	{
+		Console.ReadKey();
+	}
+}
