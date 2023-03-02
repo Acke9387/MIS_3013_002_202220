5f65ba1dce3413f309eb3418fd0d443cd034ef1d
Ackerman, Adam P <adamackrmn@gmail.com>
2023-03-02 17:37:30 -0600

    completed some in class examples over collections

---
+++ In_Class_Examples/Collections_Arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿int[] ids = new int[3];
+//double[] gpas = new double[3];
+//                0    1    2
+double[] gpas = { 3, 3.25, 2.75 };
+
+ids[0] = 33;
+ids[1] = 76;
+ids[2] = 5;
+
+Console.WriteLine("From the below list, whose GPA do you want to see? >>");
+
+//Console.WriteLine(ids[0]);
+//Console.WriteLine(ids[1]);
+//Console.WriteLine(ids[2]);
+
+//for (int i = 0; i < ids.Length; i++)
+//{
+//    int currentId = ids[i];
+//    Console.WriteLine(currentId);
+//}
+
+foreach (int currentId in ids)
+{
+    Console.WriteLine(currentId);
+}
+Console.WriteLine(">>");
+int selectedId = Convert.ToInt32(Console.ReadLine());
+
+bool foundId = false;
+
+for (int i = 0; i < ids.Length; i++)
+{
+	if (selectedId == ids[i])
+	{
+		Console.WriteLine(gpas[i]);
+		foundId = true;
+		break;
+	} 
+}
+
+if (foundId == false)
+{
+	Console.WriteLine($"Sorry, there's no student with id {selectedId.ToString("N0")}");
+}
---
+++ In_Class_Examples/Collections_Lists/Program.cs
@@ -0,0 +1,23 @@
+﻿List<string> favoriteThings = new List<string>();
+
+do
+{
+    Console.WriteLine("Please tell me on of your favorite things >>");
+    string thing = Console.ReadLine();
+
+    favoriteThings.Add(thing);
+
+    Console.WriteLine("Do you have another favorite thing to tell me about? yes or no >>");
+
+} while (Console.ReadLine().ToLower()[0] == 'y');
+
+foreach (var thing in favoriteThings)
+{
+    Console.WriteLine(thing);
+}
+
+Random random = new Random();
+int randomIndex = random.Next(0, favoriteThings.Count);
+
+Console.WriteLine($"A random thing from your list is ");
+Console.WriteLine(favoriteThings[randomIndex]);
