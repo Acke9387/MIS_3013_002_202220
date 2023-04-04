71cfd7019c2bcd69c8b48fd57d6ca035b3c68737
Ackerman, Adam P <adamackrmn@gmail.com>
2023-04-04 16:48:53 -0500

    completed speak participation

---
+++ Participations/Functions-Speak/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Please name an animal >>");
+string animal = Console.ReadLine();
+
+string noise = Speak(animal);
+Console.WriteLine(noise);
+Console.WriteLine("Enter another animal >>");
+animal = Console.ReadLine();
+Console.WriteLine(Speak(animal));
+ string Speak(string x)
+{
+    string output = "";
+    string sound;
+    if (x == "dog")
+    {
+        sound = "arf arf";
+    }
+    else if (x == "monkey")
+    {
+        sound = "oooo ahhhh ahhh";
+    }
+    else if (x == "goat")
+    {
+        sound = "Human scream";
+    }
+    else
+    {
+        sound = "GRRR";
+    }
+
+    output = $"{x} makes the sound '{sound}'";
+
+    return output;
+}
+
+//double Speak(double y, double x)
+//{
+//    return 0;
+//}
