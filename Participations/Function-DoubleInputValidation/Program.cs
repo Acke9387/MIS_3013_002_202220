b03b250f0ead3fa74d4e5d813517e35266e1a766
Ackerman, Adam P <adamackrmn@gmail.com>
2023-04-07 11:19:46 -0500

    started participation

---
+++ Participations/Function-DoubleInputValidation/Program.cs
@@ -0,0 +1,11 @@
+﻿
+double response = GetDoubleInputFromUser("Please enter your gpa >>");
+
+
+double response2 = GetDoubleInputFromUser("Please the square feet of your apartment >>");
+
+double GetDoubleInputFromUser (string msg)
+{
+
+
+}
