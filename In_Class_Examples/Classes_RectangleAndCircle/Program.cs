f51703d9c816b7c1b5dd3c2f5a063cb81b149208
Ackerman, Adam P <adamackrmn@gmail.com>
2023-04-11 17:28:02 -0500

    Completed in class exercise over Rectangle and classes

---
+++ In_Class_Examples/Classes_RectangleAndCircle/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Classes_RectangleAndCircle;
+
+Rectangle r = new Rectangle();
+r.Width = 5;
+r.Length = 10;
+//r.SetLength(10);
+
+//Console.WriteLine($"Width:{r.Width} Length:{r.Length} Area:{r.CalculateArea()} Perimeter:{r.CalculatePerimeter()}");
+
+Rectangle r1 = new Rectangle();
+r1.Width = 7.5;
+r1.Length = 10;
+
+//Console.WriteLine($"Width:{r1.Width} Length:{r1.Length} Area:{r1.CalculateArea()} Perimeter:{r1.CalculatePerimeter()}");
+
+Console.WriteLine("What is the rectangles width? >>");
+double w = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("What is the rectangles length? >>");
+double l = Convert.ToDouble(Console.ReadLine());
+
+Rectangle r2 = new Rectangle(w, l);
+
+List<Rectangle> rectangles = new List<Rectangle>();
+rectangles.Add(r);
+rectangles.Add(r1);
+rectangles.Add(r2);
+
+foreach (Rectangle rect in rectangles)
+{
+    Console.WriteLine(rect);
+}
---
+++ In_Class_Examples/Classes_RectangleAndCircle/Rectangle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_RectangleAndCircle
+{
+    public class Rectangle
+    {
+        //private double Length;
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+        public string ThisIsNotNeeded { get; set; }
+
+        //public void SetLength(double length)
+        //{
+        //    Length = length;
+        //}
+
+        //public double GetLength()
+        //{
+        //    return Length;
+        //}
+
+        /// <summary>
+        /// This is the empty/default constructor
+        /// </summary>
+        public Rectangle()
+        {
+            Length = 0;
+            Width = 0;
+            ThisIsNotNeeded = string.Empty;
+        }
+
+        /// <summary>
+        /// This is the overloaded constructor accepting the width and the length
+        /// </summary>
+        /// <param name="width">The width of the rectangle</param>
+        /// <param name="length">The length of the rectangle</param>
+        public Rectangle(double width, double length) : this()
+        {
+            Length = length;
+            Width = width;
+        }
+
+        /// <summary>
+        /// Calculates the area of the rectangle
+        /// </summary>
+        /// <returns>The area of the rectangle</returns>
+        public double CalculateArea()
+        {
+            double area = Length * Width;
+
+            return area;
+        }
+
+
+        public double CalculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+
+        public override string ToString()
+        {
+            return $"Width:{Width} \tLength:{Length} \tArea:{CalculateArea()} \tPerimeter:{CalculatePerimeter()}";
+        }
+
+    }
+}
