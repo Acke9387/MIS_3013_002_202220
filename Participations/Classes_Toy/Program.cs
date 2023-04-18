4d249ceb151a31921ef246171847ccfb8335d8ad
Ackerman, Adam P <adamackrmn@gmail.com>
2023-04-18 16:48:08 -0500

    did participation over Toy

---
+++ Participations/Classes_Toy/Program.cs
@@ -0,0 +1,23 @@
+﻿using Classes_Toy;
+
+List<Toy> toybox = new List<Toy>();
+Toy toy= new Toy();
+toy.Name = "Buzz";
+toy.Manufacturer = "Hasbro";
+toy.SetNote("Fun");
+toy.Price = 50;
+
+toybox.Add(toy);
+
+Toy toy2 = new Toy("More fun than Buzz");
+toy2.Name = "Slinky dog";
+toy2.Manufacturer = "Hasbro";
+toy2.Price = 24.99;
+
+toybox.Add(toy2);
+
+
+foreach (Toy t in toybox)
+{
+    Console.WriteLine(t.ToString());
+}
---
+++ Participations/Classes_Toy/Toy.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Toy
+{
+    public class Toy
+    {
+
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        private string Notes;
+
+        public List<string> MyProperty { get; set; }
+
+        public void SetNote(string note)
+        {
+            Notes = note;
+        }
+
+        public string GetNote()
+        {
+            return Notes;
+        }
+
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = "";
+            Price = 0;
+            Notes = string.Empty;
+            MyProperty = new List<string>();
+        }
+
+        public Toy(string note) : this()
+        {
+            //Manufacturer = string.Empty;
+            //Name = "";
+            //Price = 0;
+            //Notes = string.Empty;
+            //MyProperty = new List<string>();
+            Notes = note;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} made by {Manufacturer} costs {Price.ToString("C")} located on aisle {GetAisle()}\tNotes: {Notes}";
+        }
+
+        public string GetAisle()
+        {
+            string aisleLetter = Manufacturer.ToUpper()[0].ToString();
+            Random r = new Random();
+            aisleLetter += r.Next(1, 25);
+
+            return aisleLetter;
+        }
+    }
+}
