9d9299b3401caf9561d51bc17fbd0a1da077db86
Ackerman, Adam P <adamackrmn@gmail.com>
2023-04-25 16:54:52 -0500

    completed cars participation

---
+++ Participations/Classes_Cars/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Cars
+{
+    public class Car
+    {
+
+        public string Make { get; set; }
+        public string Model {get;set;}
+        public int Year  {get;set;}
+        public string Color {get;set;}
+        public string VIN   {get;set;}
+        public double Price {get;set;}
+
+        public Car()
+        {
+            Make = string.Empty;
+            Model = string.Empty;
+            Year  = 0;
+            Color = string.Empty;
+            VIN   = string.Empty;
+            Price = 0;
+        }
+
+        public override string ToString()
+        {
+            string output = $"{Make} made in {Year.ToString("G0")} and costs {Price.ToString("C")}";
+            return output;
+        }
+
+    }
+}
---
+++ Participations/Classes_Cars/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * 
+ *  
+ *@Professor Ackerman
+ */
+
+
+using Classes_Cars;
+
+string[] lines = File.ReadAllLines("Cars.csv");
+List<Car> automobiles = new List<Car>();
+
+for (int i = 1; i < lines.Length; i++)
+{
+    //  0    1     2    3    4    5
+    //Make,Model,Year,Color,VIN,Price
+    string[] pieces = lines[i].Split(",");
+    Car c = new Car();
+    c.Make = pieces[0];
+    c.Model = pieces[1];
+    c.Year = Convert.ToInt32(pieces[2]);
+    c.Color = pieces[3];
+    c.VIN = pieces[4];
+    c.Price = Convert.ToDouble(pieces[5]);
+
+    automobiles.Add(c);
+}
+
+foreach (Car item in automobiles)
+{
+    if (item.Year > 1999)
+    {
+        Console.WriteLine(item);
+    }
+}
