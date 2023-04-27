c946912d0a0d33e58d3a950977ad13e55662f244
Ackerman, Adam P <adamackrmn@gmail.com>
2023-04-27 17:00:17 -0500

    completed the last participation for the semester

---
+++ Participations/Classes_Pharmaceutical_Drugs/Drug.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Classes_Pharmaceutical_Drugs
+{
+    public class Drug
+    {
+        public String Name { get; set; }
+        public String Company { get; set; }
+        public String Brand { get; set; }
+        public int Code { get; set; }
+        public String StreetName { get; set; }
+
+        public Drug()
+        {
+            Name = string.Empty;
+            Company = string.Empty;
+            Brand = string.Empty;
+            Code = 0;
+            StreetName = string.Empty;
+
+        }
+
+        /// <summary>
+        /// -----------------------------
+        /// |+ | Drug (company : string)   |
+        /// |  |                        |
+        /// -----------------------------
+        /// </summary>
+        public Drug(string comp)
+        {
+            Name = string.Empty;
+            Company = comp;
+            Brand = string.Empty;
+            Code = 0;
+            StreetName = string.Empty;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{StreetName} - {Company} : {Code.ToString("G0")}";
+        }
+
+    }
+}
---
+++ Participations/Classes_Pharmaceutical_Drugs/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using Classes_Pharmaceutical_Drugs;
+
+string[] fileContents = File.ReadAllLines("Pharmaceutical Drugs.csv");
+
+List<Drug> drugList = new List<Drug>();
+
+for (int i = 1; i < fileContents.Length; i++)
+{
+    //  0     1      2     3      4
+    //Name,Company,Brand,Code,StreetName
+    string[] piecesOfLine = fileContents[i].Split(',');
+
+    Drug d = new Drug();
+    d.Name = piecesOfLine[0];
+    d.Company = piecesOfLine[1];
+    d.Brand = piecesOfLine[2];
+    d.Code = Convert.ToInt32(piecesOfLine[3]);
+    d.StreetName = piecesOfLine[4];
+
+    drugList.Add(d);
+}
+
+foreach (Drug drugItem in drugList)
+{
+    Console.WriteLine(drugItem.Name);
+}
+
+Console.WriteLine("\n\n----------------------------");
+Console.WriteLine("Which drug would you like more information about? >>");
+string answer = Console.ReadLine();
+
+foreach (Drug drugItem in drugList)
+{
+    if (drugItem.Name == answer)
+    {
+        Console.WriteLine(drugItem);
+    }
+}
